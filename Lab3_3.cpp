cdf2db2977d4c3b8fc53aa7a76b0483a9ef187ab
unknown <Jetsda_Jinaka@hotmail.com>
2019-12-27 17:26:53 +0700

    Lab3_3.cpp

--- Lab3_3.cpp
+++ Lab3_3.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-
 using namespace std;
 
-int main(){
-    cout<<"A";
+int main()
+    
+    {
+        float x=2,ans=0;
+        while(x<=69){
+            ans=ans+(1/x);
+            x++;
+        }
+        
+    cout<<ans;
     return 0;
 }
